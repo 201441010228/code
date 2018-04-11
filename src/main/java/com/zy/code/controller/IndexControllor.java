c5cc666e1ac77c6cfe4bab043268ed819ab4622b
XCoder <7101795@qq.com>
2018-04-11 13:23:25 +0800

    注册学校

--- src/main/java/com/zy/code/controller/IndexControllor.java
+++ src/main/java/com/zy/code/controller/IndexControllor.java
@@ -1,5 +1,6 @@
 package com.zy.code.controller;
 
+import com.zy.code.entity.School;
 import com.zy.code.entity.Teacher;
 import com.zy.code.utils.CodeMessageConstants;
 import com.zy.code.utils.ProcessResult;
@@ -20,7 +21,12 @@ public class IndexControllor extends BaseControllor{
 
     @RequestMapping(value = "/toregister",method = RequestMethod.GET)
     public String toregister(){
-        return "register";
+        return "registerView/register";
+    }
+
+    @RequestMapping(value = "/toregisterSchool",method = RequestMethod.GET)
+    public String toregisterSchool(){
+        return "registerView/registerSchool";
     }
 
     @RequestMapping(value = "/login",method = RequestMethod.GET)
@@ -40,6 +46,27 @@ public class IndexControllor extends BaseControllor{
         return modelAndView;
     }
 
+    @RequestMapping(value = "/registSchool",method = RequestMethod.GET)
+    public String regist(@RequestParam(value = "schoolName",required = false) String schoolName,
+                               @RequestParam(value = "province",required = false) String province,
+                               @RequestParam(value = "city",required = false) String city,
+                               @RequestParam(value = "area",required = false) String area,
+                               @RequestParam(value = "address",required = false) String address,
+                               @RequestParam(value = "phoneNumber",required = false) String phoneNumber
+                             ){
+        School school = new School();
+        school.setSchoolName(schoolName);
+        school.setProvince(province);
+        school.setCity(city);
+        school.setArea(area);
+        school.setAddress(address);
+        school.setPhoneNumber(phoneNumber);
+        ProcessResult processResult =  adminService.saveSchool(school);
+        if(processResult.getStatus().equals(CodeMessageConstants.SUCCESS.getStatus())){
+            return "register";
+        }
+        return "error";
+    }
     @RequestMapping(value = "/regist",method = RequestMethod.GET)
     public ModelAndView regist(@RequestParam(value = "teachername",required = false) String username,
                          @RequestParam(value = "password",required = false) String password,
